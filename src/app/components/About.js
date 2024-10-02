ea565a3e699024add3216e0b9bd36211b2bb998c
Leo Morgan <leomorgan@Leos-MacBook-Pro.local>
2024-10-02 22:42:32 +0200

    fixed up css stuff

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -1,6 +1,6 @@
 import styles from './About.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram, faLinkedin, faTwitter, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
 export default function AboutMe() {
   return (
@@ -8,23 +8,34 @@ export default function AboutMe() {
     <div className={styles.header}>
       <h1 className={styles.aboutHeader}>ABOUT ME</h1>
       <h1 className={styles.nameTitle}>Who Is Leo Morgan?</h1>
-      <p className={styles.subtitle}>Just a human obsessed with human performance & creativity.</p>
+      <p className={styles.subtitle}>Just a human obsessed with human potential.</p>
     </div>
   
     <div className={styles.content}>
       <div className={styles.imageWrapper}>
         <img src="/leosprint.png" alt="Leo Morgan" className={styles.profileImage} />
+        <div className={styles.socialLinks}>
+        <a href="https://instagram.com" target="_blank" className={styles.icon}>
+          <FontAwesomeIcon icon={faInstagram} size="1x" />
+        </a>
+        <a href="https://x.com" target="_blank" className={styles.icon}>
+          <FontAwesomeIcon icon={faXTwitter} size="1x" />
+        </a>
+        <a href="https://linkedin.com" target="_blank" className={styles.icon}>
+          <FontAwesomeIcon icon={faLinkedin} size="1x" />
+        </a>
+      </div>
       </div>
   
       <div className={styles.textWrapper}>
-        <h2>Hey, I'm Leo.</h2>
+        <h2 className="font-bold">Hey, I'm Leo.</h2>
         <p>
           I’m an international sprinter, software engineer, athletic performance coach, and content creator passionate about the mind, technology, and personal development.
-          I teach performance optimization, strategies for athletic development, building effective habits, and creative workflows to help individuals unlock their true potential.
+          I teach performance optimization techniques, strategies for athletic development, building effective habits, and creative workflows to help individuals unlock their true potential.
         </p>
         <p>
-          Previously, I worked as a rugby & sprints coach. Now, I share my experiences through 
-          writing and digital content, focusing on actionable insights for achieving your personal, athletics and professional goals.
+          I currently work as a Software Engineer in the financial services industry. Previously, I worked as a rugby & sprints coach as well as a freelance videographer. Now, I share my experiences and learnings through 
+          writing and digital content, focusing on actionable insights that can help you achieve your personal, athletics and professional goals.
         </p>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ import About from './components/About';
 export default function Home() {
   return (
     <div>
-      <Navbar/>    
-      <div className='p-8 items-center'>
+      <Navbar/>     
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className='items-center'>
           <Landing />
       </div>
-      <div className='p-8 items-center'>
+      <div className='items-center'>
           <About />
-      </div>   
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      </div>  
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
        <h1>This is the footer</h1>
