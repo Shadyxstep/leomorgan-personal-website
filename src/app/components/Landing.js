dd6144fe504c117b8c4420a9a2f388a73f89632e
Leo Morgan <leomorgan@Leos-MBP.station>
2025-03-17 22:10:23 +0000

    timing

--- src/app/components/Landing.js
+++ src/app/components/Landing.js
@@ -47,7 +47,7 @@ export default function HeroSection() {
           setIsVisible(true);
           setTimeout(() => {
             setIsTypingComplete(true);
-          }, 1250); 
+          }, 500); 
         }
       },
       {
